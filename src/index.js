25a473fb7f7d2711ebcafbd860d0fbdb272efa88
Medetcan Diler <medetdiler1996@gmail.com>
2023-04-20 18:42:11 +0300

    Completed assignment

--- src/index.js
+++ src/index.js
@@ -1,5 +1,22 @@
+const input = document.querySelector('#searchByID');
+const form = document.forms['form']
+
+
+
 const init = () => {
-  
+  form.addEventListener('submit', (e) => {
+    e.preventDefault();
+    const id = input.value
+    fetch(`https://jsonplaceholder.typicode.com/users/${id}`)
+      .then( res => res.json() )
+      .then( data => {
+        const title = document.querySelector('#movieDetails h4');
+        const sum = document.querySelector('#movieDetails p');
+        title.textContent = data.name;
+        sum.textContent = data.username
+      });
+    input.value = '';
+  })
 }
 
 document.addEventListener('DOMContentLoaded', init);
